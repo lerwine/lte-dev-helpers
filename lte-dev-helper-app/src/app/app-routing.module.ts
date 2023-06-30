2096dc61e526b0af8602d8261db5d0b1bc88ea1c
Leonard <lenny@erwinefamily.net>
2023-06-30 04:16:18 -0400

    Added charmap data

--- lte-dev-helper-app/src/app/app-routing.module.ts
+++ lte-dev-helper-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FontsComponent } from './fonts/fonts.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CharmapComponent } from './charmap/charmap.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "fonts", component: FontsComponent },
+  { path: 'charmap/:id', component: CharmapComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ script/Add-FontFamily.ps1
@@ -0,0 +1,207 @@
+$AssetsPath = $PSScriptRoot | Join-Path -ChildPath '..\lte-dev-helper-app\src\assets';
+
+$FontIndexPath = $AssetsPath | Join-Path -ChildPath 'font-index.json';
+[PSObject[]]$ExistingFonts = @();
+$ExistingFontNames = @();
+if ($FontIndexPath | Test-Path -PathType Leaf) {
+    $ExistingFonts = (Get-Content -LiteralPath $FontIndexPath) | ConvertFrom-Json;
+    $ExistingFontNames = @($ExistingFonts | ForEach-Object { $_.name });
+}
+
+[double]$TotalCount = [System.Windows.Media.Fonts]::SystemFontFamilies.Count;
+$Index = 0;
+if ($null -eq $Script:FontFamilies) {
+    $Script:FontFamilies = @(@([System.Windows.Media.Fonts]::SystemFontFamilies) | ForEach-Object {
+        Write-Progress -Activity 'Getting font character counts' -Status $_.FamilyNames['en-us'] -PercentComplete ([int]((([double]$Index) * 100.0) / $TotalCount));
+        $Index++;
+        $charCount = 0;
+        @($_.GetTypeFaces()) | ForEach-Object {
+            [System.Windows.Media.GlyphTypeface]$Glyph = $null;
+            if ($_.TryGetGlyphTypeface([ref]$Glyph) -and $Glyph.CharacterToGlyphMap.Count -gt $charCount) { $charCount = $Glyph.CharacterToGlyphMap.Count }
+        }
+        [PSCustomObject]@{
+            Font = $_;
+            name = $_.FamilyNames['en-us'];
+            characterCount = $charCount;
+        }
+    } | Where-Object { $_.characterCount -gt 0 -and $ExistingFontNames -notcontains $_.name });
+    Write-Progress -Activity 'Getting font character counts' -Status 'Completed' -PercentComplete 100 -Completed;
+}
+$SelectedFont = $Script:FontFamilies | ForEach-Object {
+    [PSCustomObject]@{
+        Name = $_.name;
+        'Character Count' = $_.characterCount;
+    };
+} | Out-GridView -Title 'Select font to add' -OutputMode Single;
+if ($null -ne $SelectedFont) {
+    $n = $SelectedFont.Name;
+    $id = $ExistingFontNames.Count + 1;
+    $Item = $Script:FontFamilies | Where-Object { $_.name -eq $n } | Select-Object -First 1;
+    $Script:FontFamilies = @($Script:FontFamilies | Where-Object { $_.name -ne $n });
+    $ExistingFonts += [PSCustomObject]@{
+        id = $id;
+        name = $n;
+        characterCount = $Item.characterCount;
+    };
+    
+    (ConvertTo-Json -InputObject $ExistingFonts -Depth 3) | Out-File -LiteralPath $FontIndexPath;
+    
+    if ($null -eq $Script:EntityDictionary) {
+        [System.Xml.Resolvers.XmlPreloadedResolver]$Resolver = [System.Xml.Resolvers.XmlPreloadedResolver]::new([System.Xml.Resolvers.XmlKnownDtds]::Xhtml10);
+        $Script:EntityDictionary = [System.Collections.Generic.Dictionary[int,[System.Management.Automation.PSObject]]]::new();
+        @('xhtml-lat1.ent', 'xhtml-special.ent', 'xhtml-symbol.ent') | ForEach-Object {# , 'xhtml1-transitional.dtd'
+            $EntitySet = [System.IO.Path]::GetFileNameWithoutExtension($_);
+            $Path = $PSScriptRoot | Join-Path -ChildPath $_;
+            $XmlDocument = [System.Xml.XmlDocument]::new();
+            $XmlDocument.XmlResolver = $Resolver;
+            $XmlDocument.LoadXml('<!DOCTYPE doc ['  + [System.IO.File]::ReadAllText($Path) + ']><html />');
+            $XmlDocumentType = $XmlDocument.FirstChild;
+            @($XmlDocumentType.Entities) | ForEach-Object {
+                if ($_ -is [System.Xml.XmlEntity]) {
+                    $TextNode = $null;
+                    if ($_.HasChildNodes) { $TextNode = $_.ChildNodes | Where-Object { $_ -is [System.Xml.XmlText] } | Select-Object -First 1 }
+                    if ($null -ne $TextNode) {
+                        [char]$c = $TextNode.InnerText[0];
+                        [int]$i = $c;
+                        $HasGlyph = $DisplayChars -contains $i;
+                        if (-not $HasGlyph) { $DisplayChars += $i }
+                        $Script:EntityDictionary.Add($i, ([PSCustomObject]@{
+                            Name = $_.Name;
+                            Value = $c;
+                            Encoded = "&#$i;";
+                            HasGlyph = $HasGlyph;
+                            EntitySet = $EntitySet
+                        }));
+                    } else {
+                        switch ($_.Name) {
+                            'quot' {
+                                [int]$i = ([char]'#');
+                                $HasGlyph = $DisplayChars -contains $i;
+                                if (-not $HasGlyph) { $DisplayChars += $i }
+                                $Script:EntityDictionary.Add($i, ([PSCustomObject]@{
+                                    Name = $_;
+                                    Value = ([char]'#');
+                                    Encoded = "&quot;"
+                                    HasGlyph = $HasGlyph;
+                                    EntitySet = $EntitySet
+                                }));
+                                break;
+                            }
+                            'amp' {
+                                [int]$i = ([char]'&');
+                                $HasGlyph = $DisplayChars -contains $i;
+                                if (-not $HasGlyph) { $DisplayChars += $i }
+                                $Script:EntityDictionary.Add($i, ([PSCustomObject]@{
+                                    Name = $_;
+                                    Value = ([char]'&');
+                                    Encoded = "&amp;"
+                                    HasGlyph = $HasGlyph;
+                                    EntitySet = $EntitySet
+                                }));
+                                break;
+                            }
+                            'lt' {
+                                [int]$i = ([char]'<');
+                                $HasGlyph = $DisplayChars -contains $i;
+                                if (-not $HasGlyph) { $DisplayChars += $i }
+                                $Script:EntityDictionary.Add($i, ([PSCustomObject]@{
+                                    Name = $_;
+                                    Value = ([char]'<');
+                                    Encoded = "&lt;"
+                                    HasGlyph = $HasGlyph;
+                                    EntitySet = $EntitySet
+                                }));
+                                break;
+                            }
+                            'gt' {
+                                [int]$i = ([char]'>');
+                                $HasGlyph = $DisplayChars -contains $i;
+                                if (-not $HasGlyph) { $DisplayChars += $i }
+                                $Script:EntityDictionary.Add($i, ([PSCustomObject]@{
+                                    Name = $_;
+                                    Value = ([char]'>');
+                                    Encoded = "&gt;"
+                                    HasGlyph = $HasGlyph;
+                                    EntitySet = $EntitySet
+                                }));
+                                break;
+                            }
+                            'apos' {
+                                [int]$i = ([char]"'");
+                                $HasGlyph = $DisplayChars -contains $i;
+                                if (-not $HasGlyph) { $DisplayChars += $i }
+                                $Script:EntityDictionary.Add($i, ([PSCustomObject]@{
+                                    Name = $_;
+                                    Value = ([char]"'");
+                                    Encoded = "&apos;"
+                                    HasGlyph = $HasGlyph;
+                                    EntitySet = $EntitySet
+                                }));
+                                break;
+                            }
+                            default {
+                                Write-Warning -Message "Unknown entity $_ in $EntitySet";
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    [int[]]$CharCodes = @();
+    @($Item.Font.GetTypeFaces()) | ForEach-Object {
+        [System.Windows.Media.GlyphTypeface]$Glyph = $null;
+        if ($_.TryGetGlyphTypeface([ref]$Glyph) -and $Glyph.CharacterToGlyphMap.Count -eq $Item.characterCount) {
+            [int[]]$CharCodes = @($Glyph.CharacterToGlyphMap.Keys);
+        }
+    }
+    $NonDisplayableCategories = @([System.Globalization.UnicodeCategory]::ModifierLetter, [System.Globalization.UnicodeCategory]::NonSpacingMark, [System.Globalization.UnicodeCategory]::SpaceSeparator,
+        [System.Globalization.UnicodeCategory]::LineSeparator, [System.Globalization.UnicodeCategory]::ParagraphSeparator, [System.Globalization.UnicodeCategory]::Control,
+        [System.Globalization.UnicodeCategory]::Format, [System.Globalization.UnicodeCategory]::Surrogate, [System.Globalization.UnicodeCategory]::ModifierSymbol);
+    
+    [double]$TotalCount = $CharCodes.Count;
+    $Index = 0;
+    (@($CharCodes | ForEach-Object {
+        Write-Progress -Activity 'Getting character information' -Status $_.ToString('x2') -PercentComplete ([int]((([double]$Index) * 100.0) / $TotalCount));
+        $Index++;
+        [char]$c = $_;
+        $Category = [char]::GetUnicodeCategory($c);
+        [PSObject]$Entity = $null;
+        if ([char]::IsControl($c) -or [char]::IsWhiteSpace($c) -or $NonDisplayableCategories -contains $Category) {
+            if ($Script:EntityDictionary.TryGetValue($_, [ref]$Entity)) {
+                [PSCustomObject]@{
+                    value = $_;
+                    name = $Entity.Name;
+                    encoded = $Entity.Encoded;
+                    entitySet = $Entity.EntitySet;
+                };
+            } else {
+                [PSCustomObject]@{
+                    value = $_;
+                    name = "#$_";
+                    encoded = "&#$_;";
+                };
+            }
+        } else {
+            if ($Script:EntityDictionary.TryGetValue($_, [ref]$Entity)) {
+                [PSCustomObject]@{
+                    value = $_;
+                    display = $Entity.Value;
+                    name = $Entity.Name;
+                    encoded = $Entity.Encoded;
+                    entitySet = $Entity.EntitySet;
+                };
+            } else {
+                [PSCustomObject]@{
+                    value = $_;
+                    display = $c;
+                    name = "#$_";
+                    encoded = "&#$_;";
+                };
+            }
+        }
+    }) | ConvertTo-Json -Depth 3).Replace("\u0026", "&").Replace("\u0026", "&").Replace("\u0027", "'").Replace("\u003c", "<").Replace("\u003e", ">") | Out-File -LiteralPath ($AssetsPath | Join-Path -ChildPath "char-map$id.json");
+    Write-Progress -Activity 'Getting character information' -Status 'Finished' -PercentComplete 100 -Completed;
+}
