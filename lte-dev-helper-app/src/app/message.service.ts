63c75fc81a67f2cdd5d8aa4867f6db4c2d57b27b
Leonard <lenny@erwinefamily.net>
2023-06-30 16:30:33 -0400

    modified method signatures

--- lte-dev-helper-app/src/app/message.service.ts
+++ lte-dev-helper-app/src/app/message.service.ts
@@ -6,46 +6,175 @@ const CSS_CLASS_info: string = "info";
 const CSS_CLASS_warning: string = "warning";
 const CSS_CLASS_error: string = "error";
 const CSS_CLASS_critical: string = "critical";
-
+/**
+ * Message severity levels.
+ * @export
+ * @enum {number}
+ */
 export enum MessageLevel {
+  /**
+   * Debug-level message type.
+   */
   debug,
+
+  /**
+   * Verbose message type.
+   */
   verbose,
+
+  /**
+   * Informationaal message type.
+   */
   info,
+
+  /**
+   * Warning message type.
+   */
   warning,
+
+  /**
+   * Error message type.
+   */
   error,
+
+  /**
+   * Critical error message type.
+   */
   critical
 }
 
+/**
+ * Represents an application message.
+ * @export
+ * @class Message
+ */
 export class Message {
+  /**
+   * Gets the short description of the message.
+   * @type {string}
+   * @memberof Message
+   */
   short_description: string;
+
+  /**
+   * Gets the optional detailed message.
+   * @type {(string | undefined)}
+   * @memberof Message
+   */
   details?: string;
+  
+  /**
+   * Gets the optional error associated with the message.
+   * @type {(string | Error)}
+   * @memberof Message
+   */
+  error?: Error;
+  
+  /**
+   * Gets the message severity level.
+   * @type {MessageLevel}
+   * @memberof Message
+   */
   level: MessageLevel;
-  class: string;
-  constructor(level: MessageLevel, short_description: string, details?: string) {
+  
+  /**
+   * Gets the CSS class name for the message.
+   * @type {string}
+   * @memberof Message
+   */
+  cssClass: string;
+  
+  /**
+   * Creates an instance of the Message class.
+   * @param {MessageLevel} level - The message severity level.
+   * @param {string} short_description - The short description of the message.
+   * @param {string} [details] - The optional detailed message.
+   * @memberof Message
+   */
+  constructor(level: MessageLevel, short_description: string, details?: string, error?: Error) {
     this.level = level;
     switch (level) {
       case MessageLevel.debug:
-        this.class = CSS_CLASS_debug;
+        this.cssClass = CSS_CLASS_debug;
         break;
       case MessageLevel.verbose:
-        this.class = CSS_CLASS_verbose;
+        this.cssClass = CSS_CLASS_verbose;
         break;
       case MessageLevel.warning:
-        this.class = CSS_CLASS_warning;
+        this.cssClass = CSS_CLASS_warning;
         break;
       case MessageLevel.error:
-        this.class = CSS_CLASS_error;
+        this.cssClass = CSS_CLASS_error;
         break;
       case MessageLevel.critical:
-        this.class = CSS_CLASS_critical;
+        this.cssClass = CSS_CLASS_critical;
         break;
       default:
-        this.class = CSS_CLASS_info;
+        this.cssClass = CSS_CLASS_info;
         break;
     }
     this.short_description = short_description;
     if (typeof details === 'string' && (details = details.trim()).length > 0)
+    {
       this.details = details;
+      if (typeof error === 'object' && error != null)
+        this.error = error;
+    }
+    else if (typeof error === 'object' && error != null) {
+      this.error = error;
+      var cause: string | undefined = (typeof error.cause === 'string') ? error.cause : (typeof error.cause === 'object' && error.cause != null) ? error.cause.toString() : undefined;
+      var stack: string | undefined = error.stack;
+      var name: string = error.name;
+      if (typeof (details = error.message) === 'string' && (details = details.trim()).length > 0)
+      {
+        if (typeof cause === 'string' && (cause = cause.trim()).length > 0)
+        {
+          if (typeof stack === 'string' && (stack = stack.trim()).length > 0)
+          {
+            if (typeof name === 'string' && (name = name.trim()).length > 0)
+              this.details = 'Error Name: ' + name + "\nMessage: " + details + "\n\nCause: " + cause + "\n\nStack Trace:\n" + stack;
+            else
+              this.details = 'Error Message: ' + details + "\n\nCause: " + cause + "\n\nStack Trace:\n" + stack;
+          } else if (typeof name === 'string' && (name = name.trim()).length > 0)
+            this.details = 'Error Name: ' + name + "\nMessage: " + details + "\n\nCause: " + cause;
+          else
+            this.details = 'Error Message: ' + details + "\n\nCause: " + cause;
+        }
+        else if (typeof stack === 'string' && (stack = stack.trim()).length > 0)
+        {
+          if (typeof name === 'string' && (name = name.trim()).length > 0)
+            this.details = 'Error Name: ' + name + "\nMessage: " + details + "\n\nStack Trace:\n" + stack;
+          else
+            this.details = 'Error Message: ' + details + "\n\nStack Trace:\n" + stack;
+        } else if (typeof name === 'string' && (name = name.trim()).length > 0)
+          this.details = 'Error Name: ' + name + "\nMessage: " + details;
+        else
+          this.details = 'Error Message: ' + details;
+      }
+      else if (typeof cause === 'string' && (cause = cause.trim()).length > 0)
+      {
+        if (typeof stack === 'string' && (stack = stack.trim()).length > 0)
+        {
+          if (typeof name === 'string' && (name = name.trim()).length > 0)
+            this.details = 'Error Name: ' + name + "\n\nCause: " + cause + "\n\nStack Trace:\n" + stack;
+          else
+            this.details = 'Cause: ' + cause + "\n\nStack Trace:\n" + stack;
+        } else if (typeof name === 'string' && (name = name.trim()).length > 0)
+          this.details = 'Error Name: ' + name + "\n\nCause: " + cause;
+        else
+          this.details = 'Cause: ' + cause;
+      }
+      else if (typeof stack === 'string' && (stack = stack.trim()).length > 0)
+      {
+        if (typeof name === 'string' && (name = name.trim()).length > 0)
+          this.details = 'Error Name: ' + name + "\n\nStack Trace:\n" + stack;
+        else
+          this.details = 'Stack Trace: ' + stack;
+      } else if (typeof name === 'string' && (name = name.trim()).length > 0)
+        this.details = 'Error Name: ' + name;
+      else if ((details = error.toString().trim()).length > 0)
+        this.details = details;
+    }
   }
 }
 
@@ -55,14 +184,26 @@ export class Message {
 export class MessageService {
   private _allMessages: Message[] = [];
 
+  /**
+   * Gets the messages to be displayed.
+   * @type {Message[]}
+   * @memberof MessageService
+   */
   messages: Message[] = [];
 
   // #region level Property
   
   private _level: MessageLevel = MessageLevel.info;
   
+  /**
+   * Gets or sets the minimum severity level of the messages to be displayed.
+   * @type {MessageLevel}
+   * @memberof MessageService
+   * @public
+   */
   public get level(): MessageLevel { return this._level; }
   
+  /** @type {MessageLevel} */
   public set level(value: MessageLevel) {
     if (this._level == value)
       return;
@@ -86,46 +227,80 @@ export class MessageService {
 
   constructor() { }
 
-  add(short_description: string, details?: string) {
-    var message: Message = new Message(MessageLevel.info, short_description, details);
+  private addMessage(messageLevel: MessageLevel, short_description: string, details?: string | Error) {
+    var message: Message;
+    if (typeof short_description !== 'string' || (short_description = short_description.trim()).length == 0)
+    {
+      if (typeof details === 'string')
+      {
+        if ((details = details.trim()).length == 0)
+          return;
+          var i = details.indexOf('\n');
+          message = (i < 0) ? new Message(messageLevel, details) : new Message(messageLevel, details.substring(0, i), details.substring(i));
+      }
+      else
+      {
+        if (typeof details !== 'object' || details == null)
+          return;
+        message = new Message(messageLevel, "Unexpected error", undefined, details);
+      }
+      if (typeof details !== 'string' || (details = details.trim()).length == 0)
+        return;
+    }
+    else
+      message = (typeof details === 'string') ? new Message(messageLevel, short_description, details) : new Message(messageLevel, short_description, undefined, details);
     this._allMessages.push(message);
-    if (this._level <= MessageLevel.info)
+    if (messageLevel >= this._level)
       this.messages.push(message);
   }
 
-  addDebug(short_description: string, details?: string) {
-    var message: Message = new Message(MessageLevel.debug, short_description, details);
-    this._allMessages.push(message);
-    if (this._level == MessageLevel.debug)
-      this.messages.push(message);
-  }
+  /**
+   * Adds a new informational message.
+   * @param {string} short_description - The short description of the message.
+   * @param {string} [details] - The optional message details.
+   * @memberof MessageService
+   */
+  add(short_description: string, details?: string) { this.addMessage(MessageLevel.info, short_description, details); }
 
-  addVerbose(short_description: string, details?: string) {
-    var message: Message = new Message(MessageLevel.verbose, short_description, details);
-    this._allMessages.push(message);
-    if (this._level <= MessageLevel.verbose)
-      this.messages.push(message);
-  }
+  /**
+   * Adds a new debug message.
+   * @param {string} short_description - The short description of the message.
+   * @param {string} [details] - The optional message details.
+   * @memberof MessageService
+   */
+  addDebug(short_description: string, details?: string) { this.addMessage(MessageLevel.debug, short_description, details); }
 
-  addWarning(short_description: string, details?: string) {
-    var message: Message = new Message(MessageLevel.warning, short_description, details);
-    this._allMessages.push(message);
-    if (this._level <= MessageLevel.warning)
-      this.messages.push(message);
-  }
+  /**
+   * Adds a new verbose message.
+   * @param {string} short_description - The short description of the message.
+   * @param {string} [details] - The optional message details.
+   * @memberof MessageService
+   */
+  addVerbose(short_description: string, details?: string) { this.addMessage(MessageLevel.verbose, short_description, details); }
 
-  addError(short_description: string, details?: string) {
-    var message: Message = new Message(MessageLevel.error, short_description, details);
-    this._allMessages.push(message);
-    if (this._level <= MessageLevel.error)
-      this.messages.push(message);
-  }
+  /**
+   * Adds a new warning message.
+   * @param {string} short_description - The short description of the message.
+   * @param {string | Error} [details] - The optional message details.
+   * @memberof MessageService
+   */
+  addWarning(short_description: string, details?: string | Error) { this.addMessage(MessageLevel.warning, short_description, details); }
 
-  addCritical(short_description: string, details?: string) {
-    var message: Message = new Message(MessageLevel.critical, short_description, details);
-    this._allMessages.push(message);
-    this.messages.push(message);
-  }
+  /**
+   * Adds a new error message.
+   * @param {string} short_description - The short description of the message.
+   * @param {string | Error} [details] - The optional message details.
+   * @memberof MessageService
+   */
+  addError(short_description: string, details?: string | Error) { this.addMessage(MessageLevel.error, short_description, details); }
+
+  /**
+   * Adds a new critical error message.
+   * @param {string} short_description - The short description of the message.
+   * @param {string | Error} [details] - The optional message details.
+   * @memberof MessageService
+   */
+  addCritical(short_description: string, details?: string | Error) { this.addMessage(MessageLevel.critical, short_description, details); }
 
   clear() {
     this.messages = [];
